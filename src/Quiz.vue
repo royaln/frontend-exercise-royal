438e0bc8e54926bca790e2d88e2b9c9a740431eb
Royal Nguyen <royaln@gmail.com>
2021-05-27 20:47:09 -0400

    Update Quiz.vue

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <div>{{ currentQuestion.text }}</div>
-    <div v-for="choice in currentQuestion.answers" v-bind:key="choice">
-      <input type="radio" v-model="answers[index]" :value="choice" />
-      <label for="one">{{ choice }}</label>
+  <div v-if="index != questions.length">
+    <div>
+      <div>{{ currentQuestion.text }}</div>
+      <div v-for="choice in currentQuestion.answers" v-bind:key="choice">
+        <input type="radio" v-model="answer" :value="choice">
+        <label for="one">{{ choice }}</label>
+      </div>
+      <br>
+      <span>Picked: {{ index + ":" + questions.length }}</span>
+      <span>Picked: {{ questions[index].answer }}</span>
     </div>
     <div>
-      <button v-on:click="next" v-bind:disabled="!answers[index]">Next</button>
+      <button v-on:click="next" v-bind:disabled="!answer">Next</button>
     </div>
-    <div>{{ answers[index - 1] }}</div>
+  </div>
+  <div v-else>
+    <div v-for="(a) in answers" v-bind:key="a">
+      <div>{{ a }}</div>
+    </div>
   </div>
 </template>
 
@@ -17,21 +26,23 @@ export default {
   props: {
     questions: {
       type: Array,
-      required: true,
-    },
+      required: true
+    }
   },
   methods: {
     next(event) {
+      this.answers.push(this.answer);
+      this.answer = null;
       this.currentQuestion = this.questions[++this.index];
-    },
+    }
   },
   data() {
     return {
       index: 0,
       currentQuestion: this.questions[0],
       answer: null,
-      answers: [],
+      answers: []
     };
-  },
+  }
 };
 </script>
